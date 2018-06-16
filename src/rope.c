7ba903051295bd21816ce816a9b4e5280731de74
Noah Naiman <nnaiman@bu.edu>
2018-06-16 11:24:26 -0400

    Created rope.c

---
+++ src/rope.c
@@ -0,0 +1,26 @@
+#include<stdlib.h>
+#include<stdio.h>
+
+/*
+ * Rope Definition
+ * ---------------
+ * Summary:
+ *	A special case of a binary tree wherein each leaf
+ *	contain a string. The weight of each node is the
+ * 	length of its string if a leaf, and the total weight
+ *	of all leaves in its left subtree if not.
+ *
+ * Typdef:
+ *	node
+ *
+ * Values:
+ *	char string[]
+ *		-Leaf: Characters representing some portion of text
+ *		-Non-leaf: NULL
+ *
+ *	node left
+ *		-A pointer to this node's left child node
+ *
+ *	node right
+ *		-A pointer to this node's right child node
+ */
