efc55d0bb85cd73610cb8662d7338e25be51879e
Noah Naiman <nnaiman@bu.edu>
2018-06-23 15:34:25 -0400

    Made print_chain function

--- src/piece_chain.c
+++ src/piece_chain.c
@@ -6,7 +6,8 @@
 #define BUFFERSIZE 61856
 #endif
 
-/* Piece Chain Definition
+/* 
+ * Piece Chain Definition
  * ----------------------
  * Summary:
  *	Comprising of three seperate buffers. The first
@@ -103,7 +104,7 @@ typedef struct{
 
 
 /*
- * get_file_size Function Definition
+ * get_original_size Function Definition
  * --------------------------------
  * Function Summary:
  *	Get the size in bytes of a given text file
@@ -114,14 +115,39 @@ typedef struct{
  *		-May not be NULL
  *
  * Return Type:
- *	fileLength
+ *	int fileLength
  *		-The length of the given file in bytes
  */
-int get_file_size(FILE* fileDescriptor){
+int get_original_size(FILE* fileDescriptor){
 	fseek(fileDescriptor, 0, SEEK_END);
 	int fileLength = ftell(fileDescriptor);
 	rewind(fileDescriptor);
 	return fileLength;
+}
+
+/*
+ * get_current_size Function Definition
+ * --------------------------------
+ * Function Summary:
+ *	Get the size in bytes of the current text
+ *
+ * Parameters:
+ *	PieceChain_t* chain
+ *		-A pointer to the PieceChain_t recording info for
+ *		 the current text
+ *		-May not be NULL
+ *
+ * Return Type:
+ *	int currentLength
+ *		-The current length of the text being edited
+ */
+int get_current_length(PieceChain_t* chain){
+	int currentLength = 0;
+	int i = 0;
+	for(; chain->pieces[i][2] != 0; i++){
+		currentLength += chain->pieces[i][2];
+	}
+	return currentLength;
 }
 
 /*
@@ -148,10 +174,6 @@ int get_file_size(FILE* fileDescriptor){
  *
  *	int whichBuffer
  *		-An integer representing the recorded piece's length
- *
- * Return Type:
- *	ropeNode*
- *		-A newly created rope node with no children
  */
 void record_piece(PieceChain_t* chain, int whichBuffer, int start, int length){
 	int i = 0;
@@ -175,14 +197,14 @@ PieceChain_t* init_piece_chain(char* fileName){
 	FILE *fileDescriptor = fopen(fileName, "rb");
 
 	if(fileDescriptor == NULL){
-		
+
 		newChain->original = (char *)malloc(BUFFERSIZE * sizeof(char));
 		record_piece(newChain, 0, 0, 0);
 
 		newChain->add = (char *)malloc(BUFFERSIZE * sizeof(char));
 	}
 	else{
-		int fileLength = get_file_size(fileDescriptor);
+		int fileLength = get_original_size(fileDescriptor);
 		newChain->original = (char *)malloc(fileLength * sizeof(char));
 
 		size_t lengthRead = fread(newChain->original, sizeof(char), fileLength, fileDescriptor);
@@ -197,9 +219,33 @@ PieceChain_t* init_piece_chain(char* fileName){
 	return newChain;
 }
 
+/*
+ * print_chain Function Definition
+ * --------------------------------
+ * Function Summary:
+ *	Print the recorded text to standard output
+ *
+ * Parameters:
+ *	PieceChain_t* chain
+ *		-A pointer to a PieceChain_t
+ *		-May not be NULL
+ */
+//TODO: MODIFY TO STORE OUTPUT
+void print_chain(PieceChain_t* chain){
+	int i = 0;
+	for(; chain->pieces[i][2] != 0; i++){
+		if(chain->pieces[i][0] == 0){
+			printf("%.*s", chain->pieces[i][2], &chain->original[chain->pieces[i][1]]);
+		}
+		else{
+			printf("%.*s", chain->pieces[i][2], &chain->add[chain->pieces[i][1]]);
+		}
+	}
+}
+
 int main() {
 	PieceChain_t *myChain = init_piece_chain("obsolete/rope.c");
-	printf("%s\n", myChain->original);
+	print_chain(myChain);
 	return 1;
 }
 
