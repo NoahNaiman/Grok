34ae3bfb2cf6f6331e05a69102112a7c80f8cbf7
Noah Naiman <nnaiman@bu.edu>
2018-07-02 22:51:29 -0400

    Fixed minor insertion error

--- src/splay_tree.c
+++ src/splay_tree.c
@@ -72,7 +72,7 @@ SplayTree_t* insert(SplayTree_t* root, SplayTree_t* newNode){
 	int pyhsicalFracture = root->physicalStart + preFractureLength + newNode->length;	
 	int logicalFracture = root->logicalStart + preFractureLength + newNode->length;
 	int postFractureLength = root->length - preFractureLength;
-	SplayTree_t* postFractureNode = init_splay_tree(1, pyhsicalFracture, logicalFracture, postFractureLength);
+	SplayTree_t* postFractureNode = init_splay_tree(root->buffer, pyhsicalFracture, logicalFracture, postFractureLength);
 	increment(root->right, newNode->length);
 	postFractureNode->right = root->right;
 
