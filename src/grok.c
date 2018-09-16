0dc638caefbe0b95b600b4eb6503fb297b53061a
Noah Naiman <nnaiman@bu.edu>
2018-09-16 12:40:46 -0400

    Modifying to work with pad

--- src/grok.c
+++ src/grok.c
@@ -8,6 +8,8 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <sys/ioctl.h>
+#include <unistd.h>
 
 #include "include/piece_chain.h"
 
@@ -15,26 +17,35 @@
 #define KEY_DELETE 127
 #define KEY_ESCAPE 27
 
-void init_grok(PieceChain_t *document){
+WINDOW* init_grok(PieceChain_t *document){
 	initscr();
 	raw();
 	noecho();
+	WINDOW *newPad = newpad(LINES+1, COLS);
 	nodelay(stdscr, true);
 	halfdelay(25);
 	keypad(stdscr, true);
 	mousemask(ALL_MOUSE_EVENTS | REPORT_MOUSE_POSITION, NULL);
 	mouseinterval(0);
-	printw("%s\n", document->original);
-	scrollok(stdscr, true);
-	move(0, 0);
-	refresh();
+	wprintw(newPad, "%s\n", document->original);
+	scrollok(newPad, true);
+	wmove(newPad, 0, 0);
+	prefresh(newPad, 0,0,0,0, LINES-1, COLS);
+	return newPad;
+}
+
+void get_terminal_size(int *height, int *width){
+	struct winsize window;
+    ioctl(STDOUT_FILENO, TIOCGWINSZ, &window);
+    *height = window.ws_row;
+    *width = window.ws_col;
 }
 
 //TODO: CHECK IF THERE IS ANYTHING ABOVE
-void move_up(int *cursorY, int *cursorX){
+void move_up(WINDOW *view, int *cursorY, int *cursorX, int *top, int *bottom){
 	*cursorY -= -sizeof(char);
-	move(*cursorY, *cursorX);
-	refresh()
+	wmove(view, *cursorY, *cursorX);
+	prefresh(view, *top, 0, 0, 0, *bottom, COLS);
 	if((int)(*cursorY-sizeof(char)) < 0){
 		scrl(-1);
 		move(0, *cursorX);
@@ -45,12 +56,12 @@ void move_up(int *cursorY, int *cursorX){
 	}
 }
 
-void move_down(int *cursorY, int *cursorX){
+void move_down(WINDOW *view, int *cursorY, int *cursorX, int *top, int *bottom){
 	int height;
 	int width;
-	getmaxyx(stdscr, height, width);
+	getmaxyx(view, height, width);
 	if((int)(*cursorY+sizeof(char)) >= height){
-		scroll(stdscr);
+		//scroll(stdscr);
 		move(height, *cursorX);
 	}
 	else{
@@ -111,7 +122,8 @@ void delete(int *cursorY, int *cursorX){
 	delch();
 }
 
-void handle_input(int character, int *cursorX, int *cursorY, int *logicalStart, int *pipelineIndex, char *pipelineBuffer, MEVENT event, PieceChain_t *document){
+void handle_input(WINDOW *view, int character, int *cursorX, int *cursorY, int *top, int *bottom, int *logicalStart, 
+	int *pipelineIndex, char *pipelineBuffer, MEVENT event, PieceChain_t *document){
 	switch(character){
 		case ERR:
 			if(pipelineBuffer[0] != '\0'){
@@ -135,10 +147,10 @@ void handle_input(int character, int *cursorX, int *cursorY, int *logicalStart, 
 		case KEY_ESCAPE:
 			printw("EXIT PRESSED");
 		case KEY_UP:
-			move_up(cursorY, cursorX);
+			move_up(view, cursorY, cursorX, top, bottom);
 			break;
 		case KEY_DOWN:
-			move_down(cursorY, cursorX);
+			move_down(view, cursorY, cursorX, top, bottom);
 			break;
 		case KEY_RIGHT:
 			move_right(cursorY, cursorX);
@@ -173,17 +185,22 @@ int main(int argc, char **argv){
 	PieceChain_t *document = init_piecechain(fileName);
 	MEVENT event;
 
-	init_grok(document);
-
 	int cursorX = 0;
 	int cursorY = 0;
+	int height;
+	int width;
+	get_terminal_size(&height, &width);
+	int top = 0;
+	int bottom = height;
 	int logicalStart = -1;
 	int pipelineIndex = 0;
 	char pipelineBuffer[BUFFERSIZE];
 
+	WINDOW *view = init_grok(document);
+
 	int currentChar;
-	while((currentChar = getch()) != 'q'){
-		handle_input(currentChar, &cursorX, &cursorY, &logicalStart, &pipelineIndex, pipelineBuffer, event, document);
+	while((currentChar = wgetch(view)) != 'q'){
+		handle_input(view, currentChar, &cursorX, &cursorY, &top, &bottom, &logicalStart, &pipelineIndex, pipelineBuffer, event, document);
 	}
 	endwin();
 
